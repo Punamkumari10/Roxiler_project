d93aaab1212b9b677ceb730bed7ceccba1fb5515
Punamkumari10 <140148576+Punamkumari10@users.noreply.github.com>
2025-01-11 16:41:29 +0530

    Add files via upload

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+
+class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def require_user
+    redirect_to login_path, alert: "You must be logged in to access this section." unless logged_in?
+  end
+end
